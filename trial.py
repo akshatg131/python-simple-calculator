b752f3d1dd6056f5e482697242ac43c0d048894a
akshatg131 <akshatg131@gmail.com>
2022-10-21 19:13:11 +0530

    colors added to the buttons

--- trial.py
+++ trial.py
@@ -101,7 +101,7 @@ note1 = ttk.Notebook(root)
 note1.pack(pady=5)
 
 # creating 3 frames
-area_frame = Frame(note1, width=300, height=300)
+area_frame = Frame(note1, width=300, height=300 , )
 volume_frame = Frame(note1, width=300, height=300)
 condition_frame = Frame(note1, width=300, height=300)
 area_frame.pack(fill="both", expand=1)
@@ -137,7 +137,7 @@ def show3():
 # area frame option
 shape_options = OptionMenu(
     area_frame, shapes, "Circle", "Square", "Triangle", "Rectangle").pack()
-mybutton1 = Button(area_frame, text="Select shape", command=show1).pack()
+mybutton1 = Button(area_frame, text="Select shape", command=show1, bg='red').pack()
 
 radius_side = Label(area_frame, text="Enter radius or side in m").pack()
 
@@ -153,7 +153,7 @@ height_entry.pack()
 # volume frame option
 volume_options = OptionMenu(
     volume_frame, solidshape, "Cone", "Sphere", "Cylinder", "Cube", "Cuboid").pack()
-mybutton2 = Button(volume_frame, text="Select shape", command=show2).pack()
+mybutton2 = Button(volume_frame, text="Select shape", command=show2 , bg='blue' ).pack()
 radius_side2 = Label(volume_frame, text="Enter radius or side in m").pack()
 radius_entry2 = Entry(volume_frame, font=("Helvetica", 20))
 radius_entry2.pack()
@@ -169,7 +169,7 @@ width_entry.pack()
 # condition frame
 condition_options = OptionMenu(condition_frame, conditions,
                                "Pythagorean Triplet Checker", "Complimentary&Supplementary Angles").pack()
-mybutton3 = Button(condition_frame, text="Select condition",
+mybutton3 = Button(condition_frame, bg='cyan', text="Select condition",
                    command=show3).pack()
 side1_label = Label(condition_frame, text="Enter first number").pack()
 side1_entry = Entry(condition_frame, font=("Helvetica", 20))
@@ -193,11 +193,11 @@ button_frame3 = Frame(condition_frame)
 button_frame3.pack()
 
 # creating buttons
-button1 = Button(button_frame1, text="Calculate", command=area)
+button1 = Button(button_frame1, text="Calculate", command=area , bg='yellow')
 button1.grid(row=0, column=0, padx=10)
-button2 = Button(button_frame2, text="Calculate", command=volume)
+button2 = Button(button_frame2, text="Calculate", command=volume , bg='orange')
 button2.grid(row=0, column=0, padx=10)
-button3 = Button(button_frame3, text="Calculate", command=condition_check)
+button3 = Button(button_frame3, text="Calculate", command=condition_check , bg='pink')
 button3.grid(row=0, column=0, padx=10)
 
 root.mainloop()
